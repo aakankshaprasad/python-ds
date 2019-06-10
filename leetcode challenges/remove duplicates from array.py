6890171d19e6e33eb7bb78f732d4dc4e5d2ecfa0
Aakanksha Prasad <aakankshaprasad5@gmail.com>
2019-06-11 00:49:08 +0530

    valid parenthese

---
+++ leetcode challenges/remove duplicates from array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:]=(value for value in nums if value!=val)
+        return (len(nums))
