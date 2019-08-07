c2b9c6485bcc918c52686657501f9318c7f1e321
Aakanksha Prasad <aakankshaprasad5@gmail.com>
2019-08-07 22:44:50 +0530

    search Insert

---
+++ leetcode challenges/search Insert.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        print (nums, target)
+
+        result = 0
+        for x in range(len(nums)):
+            if target == nums[x]:
+                result = x
+                break
+            elif target < nums[x]:
+                result = x
+                break
+            else:
+                result = x + 1
+
+        return result
